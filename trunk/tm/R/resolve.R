6348d3213f37f6fc2cb7cc9bf7c4b085ed694bb5
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-04-15 18:35:52 +0000

    Added ISO 639-1 code resolution for languages with available stopwords.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@737 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/tm/R/resolve.R
+++ trunk/tm/R/resolve.R
@@ -1,6 +1,16 @@
 resolveISOCode <- function(code) {
     switch(gsub("_.*", "", code),
+           "da" = "danish",
            "de" = "german",
            "en" = "english",
-           "fr" = "french")
+           "es" = "spanish",
+           "fi" = "finnish",
+           "fr" = "french",
+           "hu" = "hungarian",
+           "it" = "italian",
+           "nl" = "dutch",
+           "no" = "norwegian",
+           "pt" = "portuguese",
+           "ru" = "russian",
+           "sv" = "swedish")
 }
