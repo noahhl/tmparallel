99e53bcb4de9a08bc960162d31b4d2342a152c81
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-11-18 09:54:14 +0000

    Look up stopword .dat files at every call.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@797 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/tm/R/stopwords.R
+++ trunk/tm/R/stopwords.R
@@ -1,18 +1,18 @@
-stopwords <- local({
-    danish <- readLines(system.file("stopwords", "danish.dat", package = "tm"))
-    dutch <- readLines(system.file("stopwords", "dutch.dat", package = "tm"))
-    english <- readLines(system.file("stopwords", "english.dat", package = "tm"))
-    finnish <- readLines(system.file("stopwords", "finnish.dat", package = "tm"))
-    french <- readLines(system.file("stopwords", "french.dat", package = "tm"))
-    german <- readLines(system.file("stopwords", "german.dat", package = "tm"))
-    hungarian <- readLines(system.file("stopwords", "hungarian.dat", package = "tm"))
-    italian <- readLines(system.file("stopwords", "italian.dat", package = "tm"))
-    norwegian <- readLines(system.file("stopwords", "norwegian.dat", package = "tm"))
-    portuguese <- readLines(system.file("stopwords", "portuguese.dat", package = "tm"))
-    russian <- readLines(system.file("stopwords", "russian.dat", package = "tm"))
-    spanish <- readLines(system.file("stopwords", "spanish.dat", package = "tm"))
-    swedish <- readLines(system.file("stopwords", "swedish.dat", package = "tm"))
+stopwords <- {
     function(language = "en") {
+        danish <- readLines(system.file("stopwords", "danish.dat", package = "tm"))
+        dutch <- readLines(system.file("stopwords", "dutch.dat", package = "tm"))
+        english <- readLines(system.file("stopwords", "english.dat", package = "tm"))
+        finnish <- readLines(system.file("stopwords", "finnish.dat", package = "tm"))
+        french <- readLines(system.file("stopwords", "french.dat", package = "tm"))
+        german <- readLines(system.file("stopwords", "german.dat", package = "tm"))
+        hungarian <- readLines(system.file("stopwords", "hungarian.dat", package = "tm"))
+        italian <- readLines(system.file("stopwords", "italian.dat", package = "tm"))
+        norwegian <- readLines(system.file("stopwords", "norwegian.dat", package = "tm"))
+        portuguese <- readLines(system.file("stopwords", "portuguese.dat", package = "tm"))
+        russian <- readLines(system.file("stopwords", "russian.dat", package = "tm"))
+        spanish <- readLines(system.file("stopwords", "spanish.dat", package = "tm"))
+        swedish <- readLines(system.file("stopwords", "swedish.dat", package = "tm"))
         resolved <- resolveISOCode(language)
         lang <- if (is.null(resolved))
             language
@@ -20,4 +20,4 @@ stopwords <- local({
             resolved
         get(lang)
     }
-})
+}
