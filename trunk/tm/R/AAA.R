d341a756aaefc34630857455f474dd2e3fce2fbf
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-24 10:41:25 +0000

    Use namespace.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@862 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/tm/R/AAA.R
+++ trunk/tm/R/AAA.R
@@ -1,10 +1,10 @@
 .onLoad <- function(libname, pkgname) {
     # ToDo: Make cluster size variable!
     if (require("snow", quietly = TRUE) && require("Rmpi", quietly = TRUE) && is.null(snow::getMPIcluster()))
-        makeMPIcluster(10)
+        snow::makeMPIcluster(10)
 }
 
 .Last <- function() {
     if (require("snow", quietly = TRUE) && require("Rmpi", quietly = TRUE))
-        stopCluster(snow::getMPIcluster())
+        snow::stopCluster(snow::getMPIcluster())
 }
