6ff81f39664ec2477755be3443537409749725fa
stefan7th <stefan7th@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-01-28 10:24:27 +0000

    R-Forge transition completed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@884 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/trunk/tm/R/clustering.R
+++
@@ -1,9 +0,0 @@
-# Author: Ingo Feinerer
-
-# Distance Matrix Computation for Term Document Matrices
-# TODO: Optimize distance computation for sparse matrices (like term document matrices)
-setGeneric("dist", function(x, method = "euclidean", diag = FALSE, upper = FALSE, p = 2) standardGeneric("dist"))
-#setMethod("dist", c("termdocmatrix"),
-#          function(x, method = "euclidean", diag = FALSE, upper = FALSE, p = 2) {
-              # Do some special distance matrix computation
-#          })
