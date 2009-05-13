cafb0ab4e027f141563563afa03b4f1f0b3a191a
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-13 18:58:14 +0000

    Improve ISO 639-2 language code handling.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@949 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/stopwords.R
+++ pkg/R/stopwords.R
@@ -1,29 +1,19 @@
 # Map ISO 639-2 codes to languages used by the Snowball stemmer
 # http://en.wikipedia.org/wiki/ISO_639-2
 map_ISO_639_2 <- function(code) {
-    switch(gsub("_.*", "", code),
-           "dan" = "danish",
-           "nld" = "dutch",
-           "dut" = "dutch",
-           "eng" = "english",
-           "fin" = "finnish",
-           "fra" = "french",
-           "fre" = "french",
-           "deu" = "german",
-           "ger" = "german",
-           "hun" = "hungarian",
-           "ita" = "italian",
-           "nor" = "norwegian",
-           "por" = "portuguese",
-           "rus" = "russian",
-           "spa" = "spanish",
-           "swe" = "swedish")
+    codes <- c("dan", "nld", "dut", "eng", "fin", "fra", "fre", "deu",
+               "ger", "hun", "ita", "nor", "por", "rus", "spa", "swe")
+    names <- c("danish", "dutch", "dutch", "english", "finnish", "french",
+               "french", "german", "german", "hungarian", "italian",
+               "norwegian", "portuguese", "russian", "spanish", "swedish")
+
+    names[charmatch(gsub("_.*", "", code), codes)]
 }
 
 stopwords <- {
     function(language = "eng") {
         resolved <- tm:::map_ISO_639_2(language)
-        lang <- if (is.null(resolved))
+        lang <- if (is.na(resolved))
             language
         else
             resolved
