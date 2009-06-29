7f5ee1c6ac342b742668389a02e1cab09a6c16bb
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-29 08:41:37 +0000

    Add regression tests.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@965 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Work/regression.R
+++ pkg/Work/regression.R
@@ -2,11 +2,19 @@
 ## Regression tests
 
 library("tm")
+data("acq")
 
+# Sources and readers
 rdf <- system.file("texts", "gmane.comp.lang.r.gr.rdf", package = "tm")
 reut21578 <- system.file("texts", "reut21578", package = "tm")
 rcv1 <- system.file("texts", "rcv1", package = "tm")
-
 g <- Corpus(GmaneSource(rdf), readerControl = list(reader = readGmane))
 re <- Corpus(DirSource(reut21578), readerControl = list(reader = readReut21578XML))
 rc <- Corpus(DirSource(rcv1), readerControl = list(reader = readRCV1))
+
+# Lazy transformations
+acq <- tmMap(acq, stemDoc, lazy = TRUE)
+meta(acq, type = "corpus")
+acq[[1]]
+acq[[17]]
+meta(acq, type = "corpus")
