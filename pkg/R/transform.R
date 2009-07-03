b96c1cd59340f07b1ada47b7d934a24daa3de265
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-03 16:16:59 +0000

    Move removeCitation, removeMultipart, and removeSignature to the tau package.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@972 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/transform.R
+++ pkg/R/transform.R
@@ -4,62 +4,9 @@
 tmReduce <- function(x, tmFuns, ...)
     Reduce(function(f, ...) f(...), tmFuns, x, right = TRUE)
 
-getTransformations <- function() { c("asPlain",
-    "removeCitation", "removeMultipart", "removeNumbers",
-    "removePunctuation", "removeSignature", "removeWords",
-    "replacePatterns", "stemDoc", "stripWhitespace", "tmTolower") }
-
-setGeneric("removeMultipart",
-           function(object, ...) standardGeneric("removeMultipart"))
-setMethod("removeMultipart",
-          signature(object = "PlainTextDocument"),
-          function(object, ...) {
-              c <- Content(object)
-
-              # http://en.wikipedia.org/wiki/Multipart_message#Multipart_Messages
-              # We are only interested in text/plain parts
-              i <- grep("^Content-Type: text/plain", c)
-              r <- character(0)
-              k <- 2
-              for (j in i) {
-                  end <- if (k <= length(i)) i[k]-1 else length(c)
-                  content <- c[j:end]
-                  ## Find boundary (starting with "--")
-                  # In most cases the boundary is just one line before the Content-Type header
-                  start <- j - 1
-                  while (j > 0) {
-                      if (substr(c[j], 1, 2) == "--") {
-                          start <- j
-                          break
-                      }
-                      else
-                          j <- j - 1
-                  }
-                  index <- grep(c[start], content)
-                  index <- if (length(index) == 0) length(content) else (index[1] - 1)
-                  content <- content[1:index]
-                  # Now remove remaining headers
-                  index <- grep("^$", content)
-                  index <- if (length(index) == 0) 1 else (index[1] + 1)
-                  r <- c(r, content[index:length(content)])
-                  k <- k + 1
-              }
-
-              Content(object) <- if (length(r) == 0) c else r
-              return(object)
-          })
-
-setGeneric("removeCitation",
-           function(object, ...) standardGeneric("removeCitation"))
-# Remove e-mail citations beginning with >
-setMethod("removeCitation",
-          signature(object = "PlainTextDocument"),
-          function(object, ...) {
-              citations <- grep("^[[:blank:]]*>", Content(object))
-              if (length(citations) > 0)
-                  Content(object) <- Content(object)[-citations]
-              return(object)
-          })
+getTransformations <- function()
+    c("asPlain", "removeNumbers", "removePunctuation", "removeWords",
+    "replacePatterns", "stemDoc", "stripWhitespace", "tmTolower")
 
 setGeneric("removeNumbers", function(object, ...) standardGeneric("removeNumbers"))
 .removeNumbers <- function(object, ...) {
@@ -76,31 +23,6 @@ setGeneric("removePunctuation", function(object, ...) standardGeneric("removePun
 }
 setMethod("removePunctuation", signature(object = "PlainTextDocument"), .removePunctuation)
 #setMethod("removePunctuation", signature(object = "MinimalDocument"), .removePunctuation)
-
-setGeneric("removeSignature",
-           function(object, ...) standardGeneric("removeSignature"))
-setMethod("removeSignature",
-          signature(object = "PlainTextDocument"),
-          function(object, marks = character(0), ...) {
-              c <- Content(object)
-
-              # "---" is often added to Sourceforge mails
-              # "___" and "***" are also common, i.e.,
-              # marks <- c("^_{10,}", "^-{10,}", "^[*]{10,}")
-
-              # "-- " is the official correct signature start mark
-              marks <- c("^-- $", marks)
-
-              signatureStart <- length(c) + 1
-              for (m in marks)
-                  signatureStart <- min(grep(m, c), signatureStart)
-
-              if (signatureStart <= length(c))
-                  c <- c[-(signatureStart:length(c))]
-
-              Content(object) <- c
-              return(object)
-          })
 
 setGeneric("removeWords", function(object, words, ...) standardGeneric("removeWords"))
 .removeWords <- function(object, words, ...) {
