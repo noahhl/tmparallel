8d88c382b8eac55b3a66bfb733ca0f2e528e9860
feinerer <feinerer@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-15 15:55:02 +0000

    First attempt for weightings using SMART notation.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm@1055 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/weight.R
+++ pkg/R/weight.R
@@ -32,6 +32,59 @@ weightTfIdf <-
         if (isDTM) t(m) else m
     }, "term frequency - inverse document frequency", "tf-idf")
 
+weightSMART <-
+    WeightFunction(function(m, spec = "nnn", control = list()) {
+        if (nchar(spec) != 3)
+            stop("invalid spec")
+
+        isDTM <- inherits(m, "DocumentTermMatrix")
+        if (isDTM) m <- t(m)
+
+        term_frequency <- substr(spec, 1, 1)
+        document_frequency <- substr(spec, 2, 2)
+        normalization <- substr(spec, 3, 3)
+
+        # Term frequency
+        tf <- switch(term_frequency,
+                     # natural
+                     n = m,
+                     # logarithm
+                     l = {
+                         m$v <- 1 + log2(m$v)
+                     },
+                     # boolean
+                     b = {
+                         m$v <- rep(1, length(m$v))
+                         m
+                     })
+
+        # Document frequency
+        rs <- row_sums(m > 0)
+        if (any(rs == 0))
+            warning("term does not occur in the corpus")
+        df <- switch(term_frequency,
+                     # natural
+                     n = 1,
+                     # idf
+                     t = log2(nDocs(m) / rs),
+                     # prob idf
+                     p = max(0, log2((nDocs(m) - rs) / rs)))
+
+        # Normalization
+        norm <- switch(normalization,
+                       # none
+                       n = 1,
+                       # byte size
+                       b = control$charlength^control$alpha)
+
+        m <- tf * df
+        names(norm) <- seq_len(nDocs(m))
+        m$v <- m$v / norm[m$j]
+        m$Weighting <- paste("SMART:", spec)
+
+        if (isDTM) t(m) else m
+    }, "SMART", "SMART")
+
 weightBin <-
     WeightFunction(function(m) {
         m$v <- rep(1, length(m$v))
